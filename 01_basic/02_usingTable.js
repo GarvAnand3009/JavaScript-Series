9754dcbca84271509aa8587c4c98b550aace12cc
Garv Anand <garvanand83@gmail.com>
2025-05-24 10:41:33 +0000

    comit config done

---
+++ 01_basic/02_usingTable.js
@@ -0,0 +1,5 @@
+console.log("Hey, Today we are using Table");
+let employeeId= 2319991;
+let employeeName = "Garv Anand";
+let employeeSalary = 100000;
+console.table([employeeId, employeeName, employeeSalary]);
