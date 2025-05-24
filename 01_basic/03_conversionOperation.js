02853b0121b79f9086f4148ef1049f58b112c3f4
Garv Anand <garvanand83@gmail.com>
2025-05-24 18:06:30 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,50 @@
+//{
+
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// OUPUT is - NaN(Not a number)
+
+//}
+
+
+
+
+//{
+
+// Now we are doing that we are changing the value of score into null
+
+// let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//}
+
+
+
+
+
+// {
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+
+// // 1 => True
+// // 0 => false
+// // Empty String " " => False
+// // Filled String "Garv" => True
+
+// }
